5409de645cbc75523d8e7bf8dd52cf35880a0380
John Widmaier <johnwid92@gmail.com>
2021-03-25 21:08:37 -0400

    Added basic styling and started UI logic

--- public/scripts/add_device.js
+++ public/scripts/add_device.js
@@ -1,10 +1,27 @@
-console.log("Add Device")
+// $("#none-selected").hide()
+$("#device-fields").hide()
+$("#submit-btn").hide()
 
 $.get("http://localhost:3001/devices/supported", (data, status) => {
     data.forEach((device_type) => {
         $("#device-list").append(`<option>${device_type.device_type}</option>`)
     })
     console.log(status)
+})
+
+$("#device-list").change(() => {
+    let val = $("#device-list").val()
+    if (val == 'Select Device') {
+        $("#none-selected").show()
+        $("#device-fields").hide()
+        $("#submit-btn").hide()
+    } else if (val == 'Sonoff Basic') {
+        $("#none-selected").hide()
+        $("#device-fields").show()
+        $("#submit-btn").show()
+    } else {
+        console.log('Device not yet supported')
+    }
 })
 
 $("#submit-btn").click(() => {
